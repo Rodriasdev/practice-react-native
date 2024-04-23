b7139f69b4a9d6c789182ceaf9b4e42f1bbacc94
Rodriasd <veronrodrigo98@gmail.com>
2024-04-23 20:06:20 -0300

    environment variables were adjusted

--- server/src/db.py
+++ server/src/db.py
@@ -1,13 +1,15 @@
 import MySQLdb
 import sys
-import os
+from decouple import config
 
 def connectdb():
     try:
-        db = MySQLdb.connect(os.environ.get("DB_HOST"),os.environ.get("DB_USER"),os.environ.get("DB_PASSWORD"),os.environ.get("DB_NAME"))
+        db = MySQLdb.connect(config('DB_HOST'),config('DB_USER'),config('DB_PASSWORD'),config('DB_NAME'))
         cursor = db.cursor()
         print("Conexión correcta.")
         return cursor,db
     except MySQLdb.Error as e:
         print("No se pudo conectar a la base de datos:",e)
         sys.exit(1)
+
+connectdb()

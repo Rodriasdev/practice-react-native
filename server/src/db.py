44297051a19cc3ed6eb323cb7fce435d7f871c28
Rodriasd <veronrodrigo98@gmail.com>
2024-04-23 15:59:14 -0300

    was added readme in the server

--- server/src/db.py
+++ server/src/db.py
@@ -1,9 +1,10 @@
 import MySQLdb
 import sys
+import os
 
 def connectdb():
     try:
-        db = MySQLdb.connect("localhost","root","","app" )
+        db = MySQLdb.connect(os.environ.get("DB_HOST"),os.environ.get("DB_USER"),os.environ.get("DB_PASSWORD"),os.environ.get("DB_NAME"))
         cursor = db.cursor()
         print("Conexión correcta.")
         return cursor,db

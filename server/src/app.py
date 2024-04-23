db73d363bd1f7e223acd5959fdf084567312f471
Rodriasd <veronrodrigo98@gmail.com>
2024-04-23 20:33:41 -0300

    was added the routes for auth user

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from db import connectdb
 import MySQLdb
 import sys
@@ -8,30 +8,61 @@ app.debug = True
 
 cursor,db = connectdb()
 
-try:
-    cursor.execute("DROP TABLE IF EXISTS USERS")
-    cursor.execute("CREATE TABLE USERS (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), password VARCHAR(255))")
-except MySQLdb.error as e:
-    print(e)
-    sys.exit(1)
+# try:
+#     cursor.execute("DROP TABLE IF EXISTS USERS")
+#     cursor.execute("CREATE TABLE USERS (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), password VARCHAR(255))")
+# except MySQLdb.error as e:
+#     print(e)
+#     sys.exit(1)
 
 @app.route('/user-create', methods=['POST'])
 def insert_user():
-    try:
-        data = request.get_json()
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            print(data)
+            cursor.execute('INSERT INTO users (username,email,password) VALUES (%s,%s,%s)', (data["username"], data["email"], data["password"]))
 
-        print(data)
-        cursor.execute('INSERT INTO users (username,email,password) VALUES (%s,%s,%s)', (data["username"], data["email"], data["password"]))
+            db.commit()
+
+            return data
+
+        except MySQLdb.error as e:
+            print(e)
+            db.rollback()
+            return jsonify({'error':'error al procesar la solicitud'}), 500
+
+    else:
+        return jsonify({'error': 'Método no permitido'}), 405
 
 
-        db.commit()
+@app.route('/auth-user', methods=['PUT'])
+def auth_user():
+    if request.method == 'PUT':
+        try:
+            data = request.get_json()
 
-        return data
 
-    except MySQLdb.error as e:
-        print(e)
-        db.rollback()
-        sys.exit(1)
+            cursor.execute('SELECT id, username, email, password FROM users WHERE username=(%s)', (data["username"],))
+
+            user = cursor.fetchone()
+
+            if user:
+                return jsonify({
+                    'id': user[0],
+                    'username': user[1],
+                    'email': user[2],
+                    'password': user[3]
+                }), 200
+            else:
+                return jsonify({'error': 'Usuario no encontrado'}), 404
+
+        except MySQLdb.Error as e:
+            print(e)
+            return jsonify({'error': 'Error al procesar la solicitud'}), 500
+
+    else:
+        return jsonify({'error': 'Método no permitido'}), 405
 
 
 if __name__ == '__main__':

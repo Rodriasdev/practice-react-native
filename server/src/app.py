af12ca32452ca61ed78310f740c0e1a5aae55536
Rodriasd <veronrodrigo98@gmail.com>
2024-04-23 15:25:51 -0300

    was added the route for create the user

--- server/src/app.py
+++ server/src/app.py
@@ -1,16 +1,38 @@
-from flask import Flask
+from flask import Flask, request
 from db import connectdb
 import MySQLdb
+import sys
 
 app = Flask(__name__)
 app.debug = True
 
 cursor,db = connectdb()
 
+try:
+    cursor.execute("DROP TABLE IF EXISTS USERS")
+    cursor.execute("CREATE TABLE USERS (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), password VARCHAR(255))")
+except MySQLdb.error as e:
+    print(e)
+    sys.exit(1)
 
-@app.route('/user-create')
-def index():
-    return 'a'
+@app.route('/user-create', methods=['POST'])
+def insert_user():
+    try:
+        data = request.get_json()
+
+        print(data)
+        cursor.execute('INSERT INTO users (username,email,password) VALUES (%s,%s,%s)', (data["username"], data["email"], data["password"]))
+
+
+        db.commit()
+
+        return data
+
+    except MySQLdb.error as e:
+        print(e)
+        db.rollback()
+        sys.exit(1)
+
 
 if __name__ == '__main__':
     app.run()

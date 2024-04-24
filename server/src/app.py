3860b704898b276c12461aab899ef2b7e7db83e1
Rodriasd <veronrodrigo98@gmail.com>
2024-04-24 12:49:02 -0300

    was deleted unnecessary code

--- server/src/app.py
+++ server/src/app.py
@@ -8,19 +8,12 @@ app.debug = True
 
 cursor,db = connectdb()
 
-# try:
-#     cursor.execute("DROP TABLE IF EXISTS USERS")
-#     cursor.execute("CREATE TABLE USERS (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), password VARCHAR(255))")
-# except MySQLdb.error as e:
-#     print(e)
-#     sys.exit(1)
-
 @app.route('/user-create', methods=['POST'])
 def insert_user():
     if request.method == 'POST':
         try:
             data = request.get_json()
-            print(data)
+
             cursor.execute('INSERT INTO users (username,email,password) VALUES (%s,%s,%s)', (data["username"], data["email"], data["password"]))
 
             db.commit()
@@ -41,7 +34,6 @@ def auth_user():
     if request.method == 'PUT':
         try:
             data = request.get_json()
-
 
             cursor.execute('SELECT id, username, email, password FROM users WHERE username=(%s)', (data["username"],))
 
